25e95558d2b2f70fb079987d7cbe62ef8477d940
AndresRagua <andresragua19@gmail.com>
2023-09-05 15:12:07 -0400

    eleventh commit

--- radiosongs/settings.py
+++ radiosongs/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
 STATIC_ROOT = BASE_DIR/'assets'
-#Prueba
+#Prueba Final
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
