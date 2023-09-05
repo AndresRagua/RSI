105946aa582136d574d7e9aaa75f0c0e4457c000
AndresRagua <andresragua19@gmail.com>
2023-09-05 14:08:38 -0400

    seventh commit

--- radiosongs/settings.py
+++ radiosongs/settings.py
@@ -31,6 +31,9 @@ DEBUG = os.getenv('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 #Prueba
 
+
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -83,16 +86,31 @@ WSGI_APPLICATION = 'radiosongs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'radioSongs',
-        'USER': 'postgres',
-        'PASSWORD': '19092001',
-        'HOST': '127.0.0.1',
-        'DATABASE_PORT': '5432',
+DATABASE_URL = os.getenv('DATABASE_URL', None)
+
+if not DATABASE_URL:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'radioSongs',
+            'USER': 'postgres',
+            'PASSWORD': '19092001',
+            'HOST': '127.0.0.1',
+            'DATABASE_PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+            'OPTIONS': {'sslmode': 'require'},
+        }
+    }
 
 
 # Password validation
