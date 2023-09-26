071a2c0127c68ed6de7394f1ac0fa2c27f0a68c0
AndresRagua <andresragua19@gmail.com>
2023-09-25 22:15:48 -0400

    hilo2 commit

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from .models import *
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from .forms import *
 # Create your views here.
 
@@ -36,6 +37,20 @@ def registrar_contacto(request):
 
 def television(request):
     return render(request, 'rstv.html')
+
+# Create your views here.
+from django.shortcuts import render, redirect
+
+# imported our models
+from django.core.paginator import Paginator
+from . models import Song
+
+def index(request):
+    paginator= Paginator(Song.objects.all(),1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request,"hilos/hilo_mb.html",{"page_obj":page_obj})
+
 
 #####################################################################################################################
 
